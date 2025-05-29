b8ee80352454ac04d0771ae210d697c6a583505c
yogeshwanjari <yogeshwanjari1998@gmail.com>
2025-05-29 17:26:32 +0530

    commit xyz file

---
+++ xyz/abc.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const abc = () => {
+  return (
+    <View>
+      <Text>abc</Text>
+    </View>
+  )
+}
+
+export default abc
